4d15843e26a094a57336e61b07da042e724d6e7e
Yaqiang <yaqiang.wang@gmail.com>
2020-10-14 15:34:10 +0800

    update linint2 function package path

--- emips/spatial_alloc/spatial_allocation.py
+++ emips/spatial_alloc/spatial_allocation.py
@@ -12,7 +12,7 @@ def transform(source, source_grid, dest_grid):
     :return: (*array*) Destination data array.
     """
     if source_grid.proj == dest_grid.proj:
-        dest = np.linint2(source_grid.x_coord, source_grid.y_coord, source, dest_grid.x_coord,
+        dest = np.interpolate.linint2(source_grid.x_coord, source_grid.y_coord, source, dest_grid.x_coord,
                           dest_grid.y_coord)
     else:
         dest = geolib.reproject(source, source_grid.x_coord, source_grid.y_coord, source_grid.proj,
