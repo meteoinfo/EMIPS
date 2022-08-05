a0ebae4e1f3bb925394f4210771b1fca9732853e
chen-0126 <252941087@qq.com>
2022-08-05 09:36:35 +0800

    add scc in sectorenum

--- emips/utils/sector.py
+++ emips/utils/sector.py
@@ -4,25 +4,38 @@ __all__ = ['Sector', 'SectorEnum']
 
 class Sector(object):
 
-    def __init__(self, name):
+    def __init__(self, name, scc):
         """
         Sector class
         :param name: (*str*) Sector name.
+        :param scc: (*str*) Source classification code of sector.
         """
         self.name = name
+        self.scc = scc
 
     def __str__(self):
-        return self.name
+        re = 'Sector: {}; SCC: {}'.format(self.name, self.scc)
+        return re
 
     __repr__ = __str__
 
 #Normally used sectors
-SectorEnum = enum(ENERGY = Sector("energy"),
-               INDUSTRY = Sector("industry"),
-               RESIDENTIAL = Sector("residential"),
-               SHIPS = Sector("ships"),
-               TRANSPORT = Sector("transport"),
-               AIR = Sector("air"),
-               BIOMASS = Sector("biomass"),
-               WASTE_TREATMENT = Sector("waste_treatment"),
-               AGRICULTURE = Sector("agriculture"))
+#SectorEnum = enum(ENERGY = Sector("energy"),
+#               INDUSTRY = Sector("industry"),
+#               RESIDENTIAL = Sector("residential"),
+#               SHIPS = Sector("ships"),
+#               TRANSPORT = Sector("transport"),
+#               AIR = Sector("air"),
+#               BIOMASS = Sector("biomass"),
+#               WASTE_TREATMENT = Sector("waste_treatment"),
+#               AGRICULTURE = Sector("agriculture"))
+
+SectorEnum = enum(ENERGY = Sector("energy", '10100101'),
+               INDUSTRY = Sector("industry", "30100101"),
+               RESIDENTIAL = Sector("residential", "2104001000"),
+               SHIPS = Sector("ships", "2280000000"),
+               TRANSPORT = Sector("transport", "2294000000"),
+               AIR = Sector("air", "2275000000"),
+               BIOMASS = Sector("biomass", "2810001000"),
+               WASTE_TREATMENT = Sector("waste_treatment", "50100101"),
+               AGRICULTURE = Sector("agriculture", "28050000"))
