8d7a2dd3bfa98c5c9f6eeb361aff2ff3b15b9174
Yaqiang Wang <yqwang@cma.gov.cn>
2022-10-10 13:06:55 +0800

    in transform function when destination grid size lager than source, use griddata function with inside_mean method

--- emips/spatial_alloc/spatial_allocation.py
+++ emips/spatial_alloc/spatial_allocation.py
@@ -12,9 +12,15 @@ def transform(source, source_grid, dest_grid):
     :return: (*array*) Destination data array.
     """
     if source_grid.proj == dest_grid.proj:
-        dest = np.interpolate.linint2(source_grid.x_coord, source_grid.y_coord, source, dest_grid.x_coord,
-                          dest_grid.y_coord)
+        if source_grid.x_cell >= dest_grid.x_cell:
+            dest = np.interpolate.linint2(source_grid.x_coord, source_grid.y_coord, source, dest_grid.x_coord,
+                              dest_grid.y_coord)
+        else:
+            x, y = np.meshgrid(source_grid.x_coord, source_grid.y_coord)
+            dest = np.interpolate.griddata((x,y), source, xi=(dest_grid.x_coord, dest_grid.y_coord),
+                                           method='inside_mean')[0]
     else:
         dest = geolib.reproject(source, source_grid.x_coord, source_grid.y_coord, source_grid.proj,
                                 dest_grid.x_coord, dest_grid.y_coord, dest_grid.proj)
+
     return dest
